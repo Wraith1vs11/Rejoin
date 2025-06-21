bf50a3d4f01ffb19a528f0f876568b44fbf0abb6
Shackleton <156511971+Wraith1vs11@users.noreply.github.com>
2025-06-21 10:11:04 +0700

    Create Banana.sh

---
+++ Banana.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/termux-change-repo.sh)
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i python-pip
+pip install requests psutil prettytable pycryptodome 
