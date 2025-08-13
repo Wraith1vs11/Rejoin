067a610f83079fa61aa873f084e2f241fe0dc50f
Shackleton <156511971+Wraith1vs11@users.noreply.github.com>
2025-08-13 22:51:01 +0700

    Create OldShouko.sh

---
+++ OldShouko.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/termux-change-repo.sh)
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i python-pip
+pip install requests prettytable pycryptodome
+export CFLAGS="-Wno-error=implicit-function-declaration"
+pip install psutil
+if [ "$(uname -m)" = "aarch64" ]; then
+    curl "https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/setup.so" > ~/setup
+    chmod 755 ~/setup
+    yes | ~/setup
+	~/.bashrc
+fi
+curl -Ls "https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/OldShouko.py" -o /sdcard/Download/OldShouko.py
+if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
+    exit
+fi
