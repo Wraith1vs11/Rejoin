23d53f55ad94316080177ad5a9f0ea4ddbb74a9b
Shackleton <156511971+Wraith1vs11@users.noreply.github.com>
2025-08-14 09:13:05 +0700

    Update OldShouko.sh

--- OldShouko.sh
+++ OldShouko.sh
@@ -13,6 +13,3 @@ pip install requests rich pytz
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pip install psutil
 curl -Ls "https://raw.githubusercontent.com/Wraith1vs11/Rejoin/refs/heads/main/OldShouko.py" -o /sdcard/Download/OldShouko.py
-if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
-    exit
-fi
